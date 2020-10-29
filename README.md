b637260ae65e16247ef8e1ee8dd2bcf6725a51f5
Vinícius Knob <knob.vinicius@gmail.com>
2020-10-29 19:08:18 -0300

    Kakashi Hatake: The Copy Ninja!

---
+++ README.md
@@ -0,0 +1,8 @@
+# kakashi.js
+A Web Scraping for Financial things
+
+### Download e Instalação
+
+Você vai precisar de uma extensão como Tampermonkey para Chrome ou Gracemonkey para Firefox. 
+Depois, basta clicar no link abaixo para instalar o script na extensão:
+* [kakashi.user.js](kakashi.user.js?raw=true)
---
+++ kakashi.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Kakashi.js
+// @version      1.0.0
+// @description  A Web Scraping for Financial things
+// @author       Vinícius M. Knob <knob.vinicius@gmail.com>
+// @source       https://github.com/viniciusknob/kakashi.js
+// @match        
+// @require      https://raw.githubusercontent.com/viniciusknob/kakashi.js/main/kakashi.js
+// @grant        none
+// ==/UserScript==
